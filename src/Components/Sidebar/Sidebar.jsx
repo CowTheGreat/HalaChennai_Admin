f3f7a7e4921aa7a1b31b36be6fdc61f150d5c8be
CowTheGreat <cowara987@gmail.com>
2024-09-30 20:55:35 +0530

    routes

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -7,13 +7,19 @@ import list_product_icon from "../../assets/Product_list_icon.svg";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <Link to={"/addproduct"} style={{ textDecoration: "none" }}>
+      <Link
+        to={"https://hala-chennai-admin.vercel.app/addproduct"}
+        style={{ textDecoration: "none" }}
+      >
         <div className="sidebar-item">
           <img src={add_product_icon} alt="" />
           <p>Add Product</p>
         </div>
       </Link>
-      <Link to={"/listproduct"} style={{ textDecoration: "none" }}>
+      <Link
+        to={"https://hala-chennai-admin.vercel.app/listproduct"}
+        style={{ textDecoration: "none" }}
+      >
         <div className="sidebar-item">
           <img src={list_product_icon} alt="" />
           <p>Product List</p>
